84d6de419bb9c251cf0a50c2dc044dac9f6a7f61
TLORENT <lorent.tanguy@gmail.com>
2022-03-09 16:13:55 +0100

    [main.cpp] add get_int_from_user()

--- Simple-Cpp-Setup-main/src/main.cpp
+++ Simple-Cpp-Setup-main/src/main.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
 #include "random.hpp"
 
+int get_int_from_user();
+
 int main()
 {
-    std::cout << rand(0, 10);
+    get_int_from_user();
+}
+
+int get_int_from_user()
+{
+    int input;
+
+    while (std::cout << "Please enter a number :\n" && !(std::cin >> input)) {
+        std::cin.clear();                                                   //clear bad input flag
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); //discard input
+        std::cout << "Error : Invalid input type.\n";
+    }
+
+    return input;
 }
