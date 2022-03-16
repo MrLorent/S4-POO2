cfb23c91280e2f8961886767beb256046cde64aa
TLORENT <lorent.tanguy@gmail.com>
2022-03-16 16:29:10 +0100

    [guess_the_number.cpp] spliting code

--- Simple-Cpp-Setup-main/src/main.cpp
+++ Simple-Cpp-Setup-main/src/main.cpp
@@ -1,22 +1,6 @@
-#include <iostream>
-#include "random.hpp"
-
-int get_int_from_user();
+#include "guess_the_number.hpp"
 
 int main()
 {
-    get_int_from_user();
-}
-
-int get_int_from_user()
-{
-    int input;
-
-    while (std::cout << "Please enter a number :\n" && !(std::cin >> input)) {
-        std::cin.clear();                                                   //clear bad input flag
-        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); //discard input
-        std::cout << "Error : Invalid input type.\n";
-    }
-
-    return input;
+    play_guess_the_number();
 }
--- Simple-Cpp-Setup-main/src/myLib/random.cpp
+++ Simple-Cpp-Setup-main/src/myLib/random.cpp
@@ -7,3 +7,41 @@ int rand(unsigned min, unsigned max)
 
     return distribution(generator);
 }
+
+int head_or_tail()
+{
+    float random_value = std::rand() / float(RAND_MAX);
+
+    if (random_value < 0.5) {
+        return 0;
+    }
+    else {
+        return 1;
+    }
+}
+
+int throw_a_dice()
+{
+    float random_value = std::rand() / float(RAND_MAX);
+
+    int i = 1;
+    while (random_value > (i / 6.f) && i <= 6) {
+        i++;
+    }
+
+    return i;
+}
+
+int random_choose(std::vector<int> universe, std::vector<float> probability)
+{
+    float random_value = std::rand() / float(RAND_MAX);
+
+    int   i  = 0;
+    float p1 = probability[0];
+    while (random_value > p1 && i < int(universe.size())) {
+        i++;
+        p1 += probability[i];
+    }
+
+    return universe[i];
+}
