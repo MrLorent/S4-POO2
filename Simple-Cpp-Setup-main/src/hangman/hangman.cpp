f6135d7ff82919113e31c6dfd6c86f29f5eb3363
TLORENT <lorent.tanguy@gmail.com>
2022-03-23 20:40:27 +0100

    [hangman] functionning version 1.0

--- Simple-Cpp-Setup-main/src/hangman/hangman.cpp
+++ Simple-Cpp-Setup-main/src/hangman/hangman.cpp
@@ -8,13 +8,19 @@
 #include "hangman.hpp"
 
 const std::string pick_random_word();
-bool              player_is_alive(unsigned nb_lives);
+bool              player_is_alive(unsigned lives_count);
 bool              player_has_won(const std::vector<bool>& letters_guessed);
 bool              word_contains(const char letter, const std::string& MYSTERY_WORD);
+void              marks_as_guessed(const char letter, std::vector<bool>& letters_guessed, const std::string& MYSTERY_WORD);
+void              remove_one_life(unsigned& lives_count);
 
 void display_hangman_title();
-void display_number_of_lives(unsigned nb_lives);
+void display_number_of_lives(unsigned lives_count);
 void display_word_to_guess_with_holes(const std::string& MYSTERY_WORD, const std::vector<bool>& letters_guessed);
+void display_success_feedback(const char right_letter);
+void display_failure_feedback(const char wrong_letter);
+void display_congrats_message();
+void display_defeat_message(const std::string& MYSTERY_WORD);
 
 void play_hangman()
 {
@@ -23,36 +29,64 @@ void play_hangman()
 
     // VARIABLES
     std::vector<bool> letters_guessed(MYSTERY_WORD.size(), false);
-    unsigned          nb_lives = 8;
+    unsigned          lives_count = 8;
 
-    while (player_is_alive(nb_lives) && !player_has_won(letters_guessed)) {
-        display_hangman_title();
-        display_number_of_lives(nb_lives);
-        display_word_to_guess_with_holes(MYSTERY_WORD, letters_guessed);
+    display_hangman_title();
+    display_number_of_lives(lives_count);
+    display_word_to_guess_with_holes(MYSTERY_WORD, letters_guessed);
+    while (player_is_alive(lives_count) && !player_has_won(letters_guessed)) {
         const char guess = get_input_from_user<char>(INPUT_TYPE::CHAR);
-        std::cout << guess;
+        if (word_contains(guess, MYSTERY_WORD)) {
+            /*---- GAME ENGINE ----*/
+            marks_as_guessed(guess, letters_guessed, MYSTERY_WORD);
+
+            /*----- INTERFACE -----*/
+            display_hangman_title();
+            display_success_feedback(guess);
+            display_number_of_lives(lives_count);
+            display_word_to_guess_with_holes(MYSTERY_WORD, letters_guessed);
+        }
+        else {
+            /*---- GAME ENGINE ----*/
+            remove_one_life(lives_count);
+
+            /*----- INTERFACE -----*/
+            display_hangman_title();
+            display_failure_feedback(guess);
+            display_number_of_lives(lives_count);
+            display_word_to_guess_with_holes(MYSTERY_WORD, letters_guessed);
+        }
+    }
+    display_hangman_title();
+    if (player_has_won(letters_guessed)) {
+        std::cout << MYSTERY_WORD << "\n";
+        display_congrats_message();
+    }
+    else {
+        display_number_of_lives(lives_count);
+        display_defeat_message(MYSTERY_WORD);
     }
 }
 
 const std::string pick_random_word()
 {
     static constexpr std::array words_list = {
-        "Theobar",
-        "Flavos",
-        "Tangos",
-        "Lisouille",
-        "Jordynateur",
-        "Yayelle",
-        "Chully",
-        "Sosomaness",
+        "THEOBAR",
+        "FLAVOS",
+        "TANGOS",
+        "LISOUILLE",
+        "JORDYNATEUR",
+        "YAYELL",
+        "CHULLY",
+        "SOSOMANESS",
     };
 
     return words_list[rand<size_t>(0, words_list.size() - 1)];
 }
 
-bool player_is_alive(unsigned nb_lives)
+bool player_is_alive(unsigned lives_count)
 {
-    return (nb_lives > 0);
+    return (lives_count > 0);
 }
 
 bool player_has_won(const std::vector<bool>& letters_guessed)
@@ -62,7 +96,23 @@ bool player_has_won(const std::vector<bool>& letters_guessed)
 
 bool word_contains(const char letter, const std::string& MYSTERY_WORD)
 {
-    return (std::find(MYSTERY_WORD.begin(), MYSTERY_WORD.end(), letter) != MYSTERY_WORD.end());
+    return MYSTERY_WORD.find(std::toupper(letter)) != std::string::npos;
+}
+
+void marks_as_guessed(const char letter, std::vector<bool>& letters_guessed, const std::string& MYSTERY_WORD)
+{
+    std::string rest_of_letters(MYSTERY_WORD);
+
+    while (word_contains(letter, rest_of_letters)) {
+        size_t letter_index           = rest_of_letters.find_last_of(std::toupper(letter));
+        letters_guessed[letter_index] = true;
+        rest_of_letters               = rest_of_letters.substr(0, letter_index);
+    }
+}
+
+void remove_one_life(unsigned& lives_count)
+{
+    if (lives_count > 0) lives_count--;
 }
 
 void display_hangman_title()
@@ -80,11 +130,31 @@ void display_word_to_guess_with_holes(const std::string& MYSTERY_WORD, const std
 {
     for (size_t i = 0; i < MYSTERY_WORD.size(); ++i) {
         if (letters_guessed[i]) {
-            std::cout << toupper(MYSTERY_WORD.at(i));
+            std::cout << char(std::toupper(MYSTERY_WORD.at(i)));
         }
         else {
             std::cout << "_";
         }
     }
     std::cout << "\n\n";
+}
+
+void display_success_feedback(const char right_letter)
+{
+    std::cout << "Yes ! '" << char(std::toupper(right_letter)) << "' was a good guess !\n\n";
+}
+
+void display_failure_feedback(const char wrong_letter)
+{
+    std::cout << "No, '" << char(std::toupper(wrong_letter)) << "' isn't in the word, sorry\n\n";
+}
+
+void display_congrats_message()
+{
+    std::cout << "Congratulations ! You found the mystery word !\n";
+}
+
+void display_defeat_message(const std::string& MYSTERY_WORD)
+{
+    std::cout << "\nYou lose... The world was " << MYSTERY_WORD << "\nMaybe next time !\n ";
 }
