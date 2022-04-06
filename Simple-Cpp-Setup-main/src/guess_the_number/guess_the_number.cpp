c847272db447bc67448dd4a5beff41ee631a9c9f
TLORENT <lorent.tanguy@gmail.com>
2022-04-06 13:07:57 +0200

    [guess_the_number.cpp] display_guess_the_number_title() Fixes #2

--- Simple-Cpp-Setup-main/src/guess_the_number/guess_the_number.cpp
+++ Simple-Cpp-Setup-main/src/guess_the_number/guess_the_number.cpp
@@ -1,19 +1,18 @@
 #include "guess_the_number.hpp"
 
-int get_int_from_user();
+int  get_int_from_user();
+void display_guess_the_number_title();
 
 void play_guess_the_number()
 {
-    system("clear");
     bool game_over       = false;
     int  number_to_guess = rand(0, 100);
 
-    std::cout << "> GUESS THE NUMBER <\n\n";
+    display_guess_the_number_title();
     do {
         int user_input = get_int_from_user();
 
-        system("clear");
-        std::cout << "> GUESS THE NUMBER <\n\n";
+        display_guess_the_number_title();
 
         if (user_input == number_to_guess) {
             std::cout << "Congratulation ! You guessed it !\n";
@@ -38,10 +37,15 @@ int get_int_from_user()
     while (std::cout << "Please enter a number :\n" && !(std::cin >> input)) {
         std::cin.clear();                                                   //clear bad input flag
         std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); //discard input
-        system("clear");
-        std::cout << "> GUESS THE NUMBER <\n\n";
+        display_guess_the_number_title();
         std::cout << "Error : Invalid input type.\n";
     }
 
     return input;
+}
+
+void display_guess_the_number_title()
+{
+    system("clear");
+    std::cout << "#### GUESS THE NUMBER ####\n\n";
 }
