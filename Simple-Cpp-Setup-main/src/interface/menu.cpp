7f16222748ab958374fec7487000f20234a0d20c
TLORENT <lorent.tanguy@gmail.com>
2022-04-09 15:44:37 +0200

    [menu.cpp] improving code repartition between .hpp and .cpp

--- Simple-Cpp-Setup-main/src/interface/menu.cpp
+++ Simple-Cpp-Setup-main/src/interface/menu.cpp
@@ -5,32 +5,16 @@
 
 void display_menu_title();
 
-const std::map<GAMES, std::string> games_name = {
-    {GAMES::GUESS_THE_NUMBER, "Guess the Number"},
-    {GAMES::HANGMAN, "Hangman"},
-};
-
-const std::map<GAMES, std::string> games_command = {
-    {GAMES::GUESS_THE_NUMBER, "1"},
-    {GAMES::HANGMAN, "2"},
-};
-
-constexpr std::initializer_list<GAMES> LIST_OF_GAMES = {
-    GAMES::GUESS_THE_NUMBER,
-    GAMES::HANGMAN,
-};
-
 void show_menu()
 {
     display_menu_title();
     std::string menu = "What do you want to do ?\n";
 
-    for (auto game : LIST_OF_GAMES) {
-        menu += " " + games_command.at(game) + ". Play " + games_name.at(game) + "\n";
+    for (auto command : LIST_OF_COMMANDS) {
+        menu += " " + COMMANDS_KEYS.at(command) + ". " + COMMANDS_LABELS.at(command) + "\n";
     }
-    menu += " Q. Quit\n\n";
 
-    std::cout << menu;
+    std::cout << menu + "\n";
 }
 
 void display_menu_title()
--- Simple-Cpp-Setup-main/src/interface/menu.hpp
+++ Simple-Cpp-Setup-main/src/interface/menu.hpp
@@ -1,9 +1,28 @@
 #pragma once
 #define MENU
 
-enum class GAMES : char {
+enum class COMMANDS : char {
     GUESS_THE_NUMBER = '1',
     HANGMAN          = '2',
+    QUIT             = 'Q',
+};
+
+const std::map<COMMANDS, std::string> COMMANDS_LABELS = {
+    {COMMANDS::GUESS_THE_NUMBER, "Play Guess the Number"},
+    {COMMANDS::HANGMAN, "Play Hangman"},
+    {COMMANDS::QUIT, "Quit"},
+};
+
+const std::map<COMMANDS, std::string> COMMANDS_KEYS = {
+    {COMMANDS::GUESS_THE_NUMBER, "1"},
+    {COMMANDS::HANGMAN, "2"},
+    {COMMANDS::QUIT, "Q"},
+};
+
+constexpr std::initializer_list<COMMANDS> LIST_OF_COMMANDS = {
+    COMMANDS::GUESS_THE_NUMBER,
+    COMMANDS::HANGMAN,
+    COMMANDS::QUIT,
 };
 
 void show_menu();
