43bc790e46212e4a0ac237da0f042255c872e856
TLORENT <lorent.tanguy@gmail.com>
2022-04-09 18:15:19 +0200

    [main.cpp] implementing the switch to launch commands

--- Simple-Cpp-Setup-main/src/interface/menu.cpp
+++ Simple-Cpp-Setup-main/src/interface/menu.cpp
@@ -1,17 +1,27 @@
 #include <iostream>
-#include <map>
 
 #include "menu.hpp"
 
 void display_menu_title();
+
+bool menu_command_contains(const char command)
+{
+    for (auto command_name : list_of_options) {
+        if (command == options_commands.at(command_name) || command == char(options_commands.at(command_name) + ('a' - 'A'))) return true;
+    }
+
+    return false;
+}
 
 void show_menu()
 {
     display_menu_title();
     std::string menu = "What do you want to do ?\n";
 
-    for (auto command : LIST_OF_COMMANDS) {
-        menu += " " + COMMANDS_KEYS.at(command) + ". " + COMMANDS_LABELS.at(command) + "\n";
+    for (auto command : list_of_options) {
+        menu += " ";
+        menu += options_commands.at(command);
+        menu += ". " + options_labels.at(command) + "\n";
     }
 
     std::cout << menu + "\n";
--- Simple-Cpp-Setup-main/src/main.cpp
+++ Simple-Cpp-Setup-main/src/main.cpp
@@ -11,9 +11,32 @@ int main()
     bool quit = false;
     while (!quit) {
         show_menu();
-        const char user_choice = get_input_from_user<char>();
-        std::cout << user_choice;
-        // TODO: handle the different possible values of command with a switch
-        // NB: don't forget to handle the case of an invalid command!
+        char user_choice = get_input_from_user<char>();
+
+        while (!menu_command_contains(user_choice)) {
+            show_menu();
+
+            std::string error_message = "Error : ";
+            error_message += user_choice;
+            error_message += " isn't an option.";
+
+            std::cout << error_message + "\n";
+            user_choice = get_input_from_user<char>();
+        };
+
+        const char command = user_choice;
+
+        switch (command) {
+        case static_cast<int>(menu_options::Guess_the_number):
+            play_guess_the_number();
+            break;
+        case static_cast<int>(menu_options::Hangman):
+            play_hangman();
+            break;
+        default:
+            quit = true;
+            break;
+        }
     }
+    std::cout << "See you ;)\n";
 }
