39d63e389048c52dad21c96986c308f440480399
TLORENT <lorent.tanguy@gmail.com>
2022-04-18 16:57:24 +0200

    [random.hpp] fixing call for float with template rand()

--- assignment/src/myLib/random.hpp
+++ assignment/src/myLib/random.hpp
@@ -5,8 +5,8 @@
 template<typename T>
 T rand(T min, T max)
 {
-    static std::mt19937_64           generator(std::random_device{}());
-    std::uniform_int_distribution<T> distribution(min, max);
+    static std::mt19937_64                generator(std::random_device{}());
+    std::uniform_real_distribution<float> distribution(min, max);
 
     return distribution(generator);
 }
